2eb0fdbc2e8c548e705602dc5f21e8f290d98396
geofs-pilot <geofspilot20@gmail.com>
2025-07-04 13:48:35 -0600

    Update main.js

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ console.log("Original scripts for immersion SFX, stall buffet, carrier catapults
 function gBreath() {
    if (geofs.animation.values.loadFactor >= 3 && !geofs.pause) {
 audio.impl.html5.playFile("https://142420819-645052386429616373.preview.editmysite.com/uploads/1/4/2/4/142420819/cutgbreath.mp3")
-	}
+    }
 }
 function flankerStall() {
    if (geofs.aircraft.instance.id == 18 && geofsAddonAircraft.isSu27 == 1 && geofs.animation.values.cobraMode == 1  && !geofs.pause) {
