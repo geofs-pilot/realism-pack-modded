cfdc7fd6c217717225cf0933e9630655ae4a7168
geofs-pilot <geofspilot20@gmail.com>
2025-07-05 22:00:22 -0600

    update files

--- main.js
+++ main.js
@@ -396,7 +396,7 @@ shaLoaded = 0
 loadInterval = setInterval(function(){
 	if (shaLoaded == 0 && geofs.fx.overg.shader) {
     var scriptSHA = document.createElement("script");
-    scriptSHA.src = "https://raw.githack.com/Ariakim-Taiyo/GeoFS-Shaders-Repository/main/SSR/SSR.js";
+    scriptSHA.src = "https://raw.githack.com/geofs-pilot/realism-pack-modded/main/SSR.js";
     document.body.appendChild(scriptSHA);
     shaLoaded = 1
 	}
